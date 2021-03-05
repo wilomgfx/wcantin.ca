179a7d2c8254e9635814ba2ddd50c98fae749cf6
William Cantin <william@hugocantin.info>
2021-03-04 21:55:51 -0500

    Remove remaining css import

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,6 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/Date';
-const utilStyles = require('../../styles/utils.module.css');
 import { GetStaticProps, GetStaticPaths } from 'next';
 
 export default function Post({
@@ -20,8 +19,8 @@ export default function Post({
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1>{postData.title}</h1>
+        <div>
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
