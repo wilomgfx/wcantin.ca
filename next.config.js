cb93ce251105aef7aeb6787fe7f35b387aa89afb
William Cantin <william@hugocantin.info>
2023-02-28 16:44:49 -0500

    Allowing tests inside page folder and fix build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // https://www.benmvp.com/blog/putting-test-files-pages-folder-nextjs-app/
+  pageExtensions: [
+    // `.page.tsx` for page components
+    "page.tsx",
+    // `.api.ts` for API routes
+    "api.ts",
+  ],
+};
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Home from "./";
+import Home from "./index.page";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
