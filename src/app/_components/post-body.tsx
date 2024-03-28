f526092483b8932b1ddfed584735671ff6080728
William Cantin <william@wcantin.ca>
2024-03-27 21:53:01 -0400

    Add eslint, prettier, format everything and add daisyUI

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,16 +1,16 @@
-import markdownStyles from "./markdown-styles.module.css";
+import markdownStyles from './markdown-styles.module.css'
 
 type Props = {
-  content: string;
-};
+    content: string
+}
 
 export function PostBody({ content }: Props) {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  );
+    return (
+        <div className="max-w-2xl mx-auto">
+            <div
+                className={markdownStyles['markdown']}
+                dangerouslySetInnerHTML={{ __html: content }}
+            />
+        </div>
+    )
 }
